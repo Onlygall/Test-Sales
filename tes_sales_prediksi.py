33ade02a6847d228922d7ae3c9cdbb8756e55052
Galeh Saputra <gall@students.unnes.ac.id>
2025-05-31 12:58:28 +0700

    tes_sales_prediksi.py

--- tes_sales_prediksi.py
+++ tes_sales_prediksi.py
@@ -7,10 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/17U1P8Tp8cgpMamW7zCZ-MStCAcGByntM
 """
 
-#from google.colab import drive
-#drive.mount('/content/drive')
-
-#!pip install streamlit
 
 import streamlit as st
 import pandas as pd
